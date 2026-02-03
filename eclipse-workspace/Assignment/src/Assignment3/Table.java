2b193fa93ead0fe466dca98584b01d313652d8d3
ShindeKranti <krantishinde28@gmail.com>
2026-02-03 23:15:20 +0530

    Added Assignment3 folder

---
+++ eclipse-workspace/Assignment/src/Assignment3/Table.java
@@ -0,0 +1,12 @@
+package Assignment3;
+
+public class Table {
+    public static void main(String[] args) {
+        int num = 5;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num * i);
+        }
+    }
+}
+

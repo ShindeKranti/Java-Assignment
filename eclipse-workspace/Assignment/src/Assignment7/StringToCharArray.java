072f97025cfc2d51c9f2b7ee38c2d68d5b4f42ab
ShindeKranti <krantishinde28@gmail.com>
2026-02-09 11:50:18 +0530

    Added  Assignment7 folder

---
+++ eclipse-workspace/Assignment/src/Assignment7/StringToCharArray.java
@@ -0,0 +1,18 @@
+package Assignment7;
+
+import java.util.Scanner;
+
+public class StringToCharArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+
+        char[] ch = str.toCharArray();
+
+        System.out.println("Characters are:");
+        for (int i = 0; i < ch.length; i++) {
+            System.out.println(ch[i]);
+        }
+    }
+}

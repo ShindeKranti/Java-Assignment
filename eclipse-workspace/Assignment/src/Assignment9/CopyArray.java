f90821afbb2006bdb619cd122ea06912cb2a8460
ShindeKranti <krantishinde28@gmail.com>
2026-02-10 22:43:32 +0530

    Added Assignment9 folder

---
+++ eclipse-workspace/Assignment/src/Assignment9/CopyArray.java
@@ -0,0 +1,18 @@
+package Assignment9;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = new int[arr1.length];
+
+        for(int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i];
+        }
+
+        System.out.println("Copied Array:");
+        for(int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+    }
+}
+
---
+++ eclipse-workspace/Assignment/src/Assignment9/EvenOddCount.java
@@ -0,0 +1,19 @@
+package Assignment9;
+
+public class EvenOddCount {
+    public static void main(String[] args) {
+        int[] arr = {10, 15, 20, 25, 30};
+        
+        int even = 0, odd = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        System.out.println("Even count: " + even);
+        System.out.println("Odd count: " + odd);
+    }
+}
